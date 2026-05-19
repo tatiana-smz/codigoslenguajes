2e1a14e53448a1799b3227d12d3a345e2354c86e
tatiana-smz <tatiana.mz.est@gmail.com>
2026-05-18 21:38:37 -0500

    Create codigo.scala

---
+++ trabajos codigos lenguajes/codigo.scala
@@ -0,0 +1,12 @@
+object codigo {
+    def main(args: Array[String]): Unit = {
+    val nombre = "Mundo"
+    println(s"¡Hola, $nombre! Bienvenido a Scala")
+
+    // Suma sencilla
+    val a = 10
+    val b = 20
+    println(s"La suma de $a + $b = ${a + b}")
+  }
+}
+ 
