db3e8663b6e86e9cacd585703edc2bc338babaae
tatiana-smz <tatiana.mz.est@gmail.com>
2026-05-18 21:39:14 -0500

    Create codigo.rs

---
+++ trabajos codigos lenguajes/codigo.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let nombre = "Mundo";
+    println!("¡Hola, {}! Bienvenido a Rust", nombre);
+ 
+    // Suma sencilla
+    let a: i32 = 10;
+    let b: i32 = 20;
+    println!("La suma de {} + {} = {}", a, b, a + b);
+}
+ 
